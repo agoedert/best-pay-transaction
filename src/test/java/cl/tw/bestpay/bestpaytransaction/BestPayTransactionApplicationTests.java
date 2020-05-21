b90f4eb1752a209f1cd9d0db9d5ac4d35f1ce2bd
Alexandre Goedert <alexandre.goedert@thoughtworks.com>
2020-05-21 18:06:43 -0400

    Initial commit

---
+++ src/test/java/cl/tw/bestpay/bestpaytransaction/BestPayTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.tw.bestpay.bestpaytransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestPayTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
